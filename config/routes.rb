44c25887b9b8e0b50c4c19ea180ef26f31581d7d
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-26 16:29:56 +0900

    rails g controller web #14

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'web/index'
+  get 'web/about'
+  get 'web/contact'
   devise_for :companies
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
