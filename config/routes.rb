3e528864e19079feadf3a7e92de7eef18ec9c42b
S-H-GAMELINKS <gamelinks007@gmail.com>
2019-01-26 16:33:30 +0900

    using /contact #17

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'web#index'
   get 'about', to: 'web#about'
-  get 'web/contact'
+  get 'contact', to: 'web#contact'
   devise_for :companies
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
